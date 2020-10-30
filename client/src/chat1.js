7e0c5a31f0850bb5ba7ca14f2f89e5cf5b732568
Emily <emilysaber13@gmail.com>
2020-10-30 16:02:02 -0400

    end of class Friday commit, need to work on array in react

--- client/src/chat1.js
+++ client/src/chat1.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
 
 function ChatOne() {
-  const [chat, setChat] = useState('')
+  const [chat, setChat] = useState(null);
+
   useEffect(() => {
-    fetch("/chatroom")
+    fetch("/mainchat")
       .then((response) => response.json())
       .then((chatMessage) => {
-        let chat = [];
+        let chatArray = [];
         chatMessage.forEach((message) => {
-          chat.push(`${message.name}: ${message.message}`)
+          chatArray.push(`${message.name}: ${message.message}`);
+          setChat(chatArray);
+          console.log(chatArray);
         });
       });
-  });
+  }, []);
+
   return (
     <div>
       <body class="body">
-        <div class="chatDisplay">Chat Room One{chat}</div>
+        <div class="chatDisplay">{chat}</div>
         <form class="textBox">
-          <input type="text" placeholder="username"/>
+          <input type="text" placeholder="username" />
           <textarea class="text" type="text" placeholder="Type Message Here" />
           <input type="submit" value="send" class="buttons" />
           <input type="submit" value="refresh" class="buttons" />
